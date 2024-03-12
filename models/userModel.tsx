7ffca8f8b85e13f09d526e82dcfb92a857c2f1bd
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-03-13 00:28:55 +0530

    added database

---
+++ models/userModel.tsx
@@ -0,0 +1,37 @@
+import mongoose, { Model } from 'mongoose';
+
+export interface IUser{
+    username:string;
+    fullName:string;
+    email: string;
+    avatar?:string;
+}
+
+export interface IUserDocument extends IUser, mongoose.Document{
+    createdAt: Date;
+    updatedAt: Date;
+}
+const userSchema = new mongoose.Schema<IUserDocument>({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatar:{
+        type: String,
+        default: ""
+    }
+},{timestamps:true});
+
+const User:Model<IUserDocument> = mongoose.models?.User || mongoose.model("User", userSchema);
+
+export default User;
