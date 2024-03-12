f4f9c85049a28dfacc7602cbfae40130ca2ab9ba
rishantkohli2033 <rishantkohli2033@gmail.com>
2024-03-13 01:53:06 +0530

    auth setup

---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+import mongoose, { Connection } from "mongoose";
+let cachedConnection:Connection | null = null
+export async function connectToMongoDB(){
+    if(cachedConnection){
+        console.log("Using Cached Connection");
+        return cachedConnection;
+    }
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI as string);
+        cachedConnection = conn.connection;
+        console.log("New MongoDB connection established");
+        return cachedConnection;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
